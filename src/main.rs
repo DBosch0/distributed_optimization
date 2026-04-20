7ddbd33143e8e922a9ceab00e2b0344efa01273a
DavidBosch-Code <davidbos@chalmers.se>
2026-04-20 16:45:23 +0200

    finished with DGD, multithreading

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,19 @@
 // 3. generate local data, and share the data in optimization
 // 4. generate output data and save to files -> python plotting.
 
-use std::ops::Sub;
+use std::{
+    ops::Sub,
+    sync::{Arc, atomic::AtomicUsize, atomic::Ordering, mpsc},
+    thread,
+    time::Duration,
+};
 
+use atomic_float::AtomicF32;
 use nalgebra::{self as na};
 use rand::distr::{Bernoulli, Distribution};
+
+const EPS: f32 = 0.0001;
+
 struct Graph {
     adjacency_matrix: na::DMatrix<i8>,
 }
@@ -29,9 +38,13 @@ impl Graph {
         let tau = m as f32 + 1.0;
         let lap = d.sub(&self.adjacency_matrix).cast::<f32>();
         let w = na::DMatrix::identity(lap.nrows(), lap.ncols()) - lap / tau;
-        // assert_eq!(w, w.transpose(), "w must be symmetric");
-        // let ones = na::DVector::from_element(w.ncols(), 1.0);
-        // assert_eq!(&w * &ones, ones, "W must be row stochastic");
+        assert_eq!(&w, &w.transpose(), "w must be symmetric");
+        let ones = na::DVector::from_element(w.ncols(), 1.0);
+
+        assert!(
+            (&w * &ones - &ones).iter().all(|elem| elem.abs() < EPS),
+            "W must be row stochastic"
+        );
         w
     }
 }
@@ -51,128 +64,206 @@ fn generate_graph(n: usize, threshold: f64) -> Graph {
 }
 
 trait ObjectiveFunction {
-    fn obj(&self, x: &na::DMatrix<f32>) -> f32;
-    fn grad(&self, x: &na::DMatrix<f32>) -> na::DMatrix<f32>;
+    fn obj(&self, x: &na::DVector<f32>) -> f32;
+    fn grad(&self, x: &na::DVector<f32>) -> na::DVector<f32>;
 }
 
+trait OptAlg {
+    fn step(&mut self);
+    fn res(&mut self, res: &Arc<[AtomicF32]>);
+}
+
+#[derive(Debug)]
 struct LeastSquares {
     n: usize,
     p: usize,
-    ms: Vec<na::DMatrix<f32>>,
-    ys: Vec<na::DVector<f32>>,
+    m: na::DMatrix<f32>,
+    y: na::DVector<f32>,
     // true_x: na::DVector<f32>,
 }
 
 impl LeastSquares {
-    fn new(n: usize, p: usize, mis: &[usize], true_x: &na::DVector<f32>) -> Self {
-        // assert_eq!(true_x.nrows(), n);
+    fn new(n: usize, p: usize, mi: usize, true_x: &na::DVector<f32>) -> Self {
         assert_eq!(true_x.nrows(), p);
-        assert_eq!(mis.len(), n);
 
-        let mut ms = Vec::new();
-        let mut ys = Vec::new();
-        for (i, mi) in mis.into_iter().enumerate() {
-            ms.push(na::DMatrix::<f32>::new_random(*mi, p));
-            let noise = na::DVector::<f32>::new_random(*mi);
-            ys.push(&ms[i] * true_x + noise);
-        }
+        let m = na::DMatrix::<f32>::new_random(mi, p);
+        let noise = na::DVector::<f32>::new_random(mi);
+        let y = &m * true_x + noise;
 
-        Self {
-            n,
-            p,
-            ms,
-            // true_x,
-            ys,
-        }
+        Self { n, p, m, y }
     }
 }
 
 impl ObjectiveFunction for LeastSquares {
-    fn obj(&self, x: &nalgebra::DMatrix<f32>) -> f32 {
-        assert_eq!(x.nrows(), self.n);
-        assert_eq!(x.ncols(), self.p);
-
-        let mut s = 0.0;
-        for i in 0..self.n {
-            s += (&self.ms[i] * &x.row(i).transpose() - &self.ys[i]).norm();
-        }
-
-        s / 2.0 / self.n as f32
+    fn obj(&self, x: &nalgebra::DVector<f32>) -> f32 {
+        assert_eq!(x.nrows(), self.p);
+        (&self.m * x - &self.y).norm() / 2.0 / self.n as f32
     }
 
-    fn grad(&self, x: &nalgebra::DMatrix<f32>) -> nalgebra::DMatrix<f32> {
-        assert_eq!(x.nrows(), self.n);
-        assert_eq!(x.ncols(), self.p);
-        let mut out = na::DMatrix::zeros(x.nrows(), x.ncols());
-        for i in 0..self.n {
-            let row = &self.ms[i].transpose() * (&self.ms[i] * &x.row(i).transpose() - &self.ys[i]);
-            out.set_row(i, &row.transpose())
-        }
-        out / self.n as f32
+    fn grad(&self, x: &nalgebra::DVector<f32>) -> nalgebra::DVector<f32> {
+        assert_eq!(x.nrows(), self.p);
+        &self.m.transpose() * (&self.m * x - &self.y) / self.n as f32
     }
 }
 
-struct DGD<F: ObjectiveFunction, G>
-where
-    G: Fn(f32, usize) -> f32,
-{
-    w: na::DMatrix<f32>,
-    x_0: na::DMatrix<f32>,
-    x: na::DMatrix<f32>,
-    x_true: na::DMatrix<f32>,
-    alpha_zero: f32,
+#[derive(Debug)]
+struct DGDNode<O: ObjectiveFunction, G: Fn(usize, f32) -> f32> {
+    id: usize,
+    n: usize,
+    obj: O,
+    x: na::DVector<f32>,
+    x_true: na::DVector<f32>,
+    w: na::DVector<f32>,
     k: usize,
-    f: F,
-    alpha_update_rule: G,
+    alpha_zero: f32,
+    alpha_update: G,
+    send: Box<[mpsc::Sender<(usize, na::DVector<f32>)>]>,
+    recv: mpsc::Receiver<(usize, na::DVector<f32>)>,
+    sync: Arc<AtomicUsize>,
 }
 
-impl<F: ObjectiveFunction, G: Fn(f32, usize) -> f32> DGD<F, G> {
+unsafe impl<O: ObjectiveFunction, G: Fn(usize, f32) -> f32> Send for DGDNode<O, G> {}
+unsafe impl<O: ObjectiveFunction, G: Fn(usize, f32) -> f32> Sync for DGDNode<O, G> {}
+
+impl<O: ObjectiveFunction, G: Fn(usize, f32) -> f32 + Copy> DGDNode<O, G> {
     fn new(
         graph: Graph,
-        x_0: na::DMatrix<f32>,
-        x_true: na::DMatrix<f32>,
+        mut objs: Vec<O>,
+        x_0: na::DVector<f32>,
+        x_true: na::DVector<f32>,
         alpha_zero: f32,
-        f: F,
-        alpha_update_rule: G,
-    ) -> Self {
-        Self {
-            w: graph.weight_matrix(),
-            x_0: x_0.clone(),
-            x: x_0,
-            x_true,
-            alpha_zero,
-            k: 0,
-            f,
-            alpha_update_rule,
-        }
-    }
+        alpha_update: G,
+        sync: Arc<AtomicUsize>,
+    ) -> Box<[Self]> {
+        let n = graph.adjacency_matrix.nrows();
+        assert!(objs.len() == n);
 
+        let mut senders: Vec<Vec<mpsc::Sender<(usize, na::DVector<f32>)>>> = vec![Vec::new(); n];
+        let mut receivers: Vec<mpsc::Receiver<(usize, na::DVector<f32>)>> = Vec::with_capacity(n);
+
+        let w = graph.weight_matrix();
+
+        for i in 0..n {
+            let (sender, receiver) = mpsc::channel();
+            receivers.push(receiver);
+            for j in 0..n {
+                if i == j {
+                    continue;
+                }
+                if w[(i, j)].abs() < EPS {
+                    continue;
+                }
+                senders[j].push(sender.clone());
+            }
+        }
+
+        let mut nodes = Vec::with_capacity(n);
+        for i in 0..n {
+            let node = DGDNode {
+                id: n - i - 1,
+                n,
+                obj: objs.pop().expect("valid for all n"),
+                // x_0: x_0.clone(),
+                x: x_0.clone(),
+                x_true: x_true.clone(),
+                w: w.row(n - i - 1).clone().transpose(),
+                k: 0,
+                alpha_zero,
+                alpha_update,
+                send: senders.pop().expect("valid of n").into_boxed_slice(),
+                recv: receivers.pop().expect("valid of n"),
+                sync: sync.clone(),
+            };
+            nodes.push(node);
+        }
+
+        nodes.into_boxed_slice()
+    }
+}
+
+impl<O: ObjectiveFunction, G: Fn(usize, f32) -> f32> OptAlg for DGDNode<O, G> {
     fn step(&mut self) {
         self.k += 1;
-        let alpha = (self.alpha_update_rule)(self.alpha_zero, self.k);
-        self.x = &self.w * &self.x - alpha * self.f.grad(&self.x);
+        for s in self.send.iter() {
+            s.send((self.id, self.x.clone())).expect("can indeed send");
+        }
+
+        let mut new_x = self.w[self.id] * &self.x
+            - (self.alpha_update)(self.k, self.alpha_zero) * self.obj.grad(&self.x);
+        for _ in 0..self.send.len() {
+            let (id, other_x) = self.recv.recv().expect("values are being sent");
+            assert!(id != self.id);
+            new_x += self.w[id] * other_x;
+        }
+        self.x = new_x;
+        self.sync.fetch_add(1, Ordering::Relaxed);
     }
 
-    fn res(&self) -> f32 {
-        (&self.x - &self.x_true).norm() / (&self.x_0 - &self.x_true).norm()
+    fn res(&mut self, res: &Arc<[AtomicF32]>) {
+        res.get(self.k)
+            .expect("do not go beyong ITERATIONS len")
+            .fetch_add((&self.x - &self.x_true).norm_squared(), Ordering::Relaxed);
     }
 }
 
 fn main() {
-    let p = 5;
+    let p = 4;
     let n = 10;
     let graph = generate_graph(n, 0.5);
-    let x_0 = na::DMatrix::zeros(n, p);
-    let x_true = na::DMatrix::from_element(n, p, 300.0);
-    // let x_true = na::DVector::from_element(p, 300.0);
-    let ls = LeastSquares::new(n, p, &vec![1; n], &x_true.row(0).transpose());
-    println!("{}", ls.grad(&na::DMatrix::from_element(n, p, 0.0)));
+    let x_0 = na::DVector::<f32>::zeros(p);
+    let x_true = na::DVector::from_element(p, 300.0);
+    let objs = (0..n)
+        .map(|_| LeastSquares::new(n, p, 2, &x_true))
+        .collect::<Vec<_>>();
 
-    let mut dgd = DGD::new(graph, x_0, x_true, 0.5276, ls, |alpha_0, k| {
-        3.0 * alpha_0 / (k as f32).powf(1.0 / 3.0)
-    });
-    for k in 1..=3000 {
-        dgd.step();
-        println!("{}:{}", k, dgd.res());
+    let sync = Arc::new(AtomicUsize::new(0));
+
+    let nodes = DGDNode::new(
+        graph,
+        objs,
+        x_0,
+        x_true,
+        0.5,
+        |k, alpha| 3.0 * alpha / (k as f32).powf(1.0 / 3.0),
+        Arc::clone(&sync),
+    );
+
+    const ITERATIONS: usize = 3000;
+    let res = (0..ITERATIONS + 1)
+        .map(|_| AtomicF32::new(0.0))
+        .collect::<Arc<[_]>>();
+
+    let mut handles = Vec::new();
+    for mut node in nodes.into_iter() {
+        let res_handle = Arc::clone(&res);
+        handles.push(thread::spawn(move || {
+            node.res(&res_handle);
+            while node.k < ITERATIONS {
+                while node.sync.load(Ordering::Relaxed) < node.k * node.n {
+                    std::thread::sleep(Duration::from_nanos(1));
+                }
+                node.step();
+                node.res(&res_handle);
+            }
+        }));
+    }
+
+    for handle in handles {
+        handle.join().expect("things went well");
+    }
+
+    let res0 = res
+        .get(0)
+        .expect("value exists")
+        .load(Ordering::Relaxed)
+        .sqrt();
+    for i in 1..res.len() {
+        let v = res
+            .get(i)
+            .expect("value exists")
+            .load(Ordering::Relaxed)
+            .sqrt()
+            / res0;
+        println!("k: {i}, v:{v}")
     }
 }
